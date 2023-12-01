090d40eec2d3f89470ae859c0f3bcfa379386a70
Markus Unterwaditzer <markus-tarpit+git@unterwaditzer.net>
2023-12-01 23:39:07 +0100

    reduce calls to sed

--- day1.bash
+++ day1.bash
@@ -1,14 +1,10 @@
 #!/bin/bash
 
 run() (
-    input="$(cat)"
-    
-    paste \
-        <(echo -n "$input" | sed -e 's/^[a-z]*\([0-9]\).*$/\1/g') \
-        <(echo -n "$input" | sed -e 's/^.*\([0-9]\)[a-z]*$/\1/g') \
-        | tr -d $'\t' \
-        | tr $'\n' '+' \
-        | sed -e 's/+$//g' \
+    sed \
+        -e 's/^\([a-z]*\([0-9]\).*\)$/\2\1/g' \
+        -e 's/^\(\(.\).*\([0-9]\)[a-z]*\)$/\2\3/g' \
+        | paste -sd+ - \
         | bc
 )
 
@@ -22,7 +18,7 @@ cat day1.txt \
     $(
         i=0
         for f in one two three four five six seven eight nine; do
-            (( i+=1 ))
+            (( i += 1 ))
             echo "-e s/$f/$f$i$f/g"
         done
     ) \
